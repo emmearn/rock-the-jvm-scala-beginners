91b044fe66a76ca0b4629d1ca7fc0a6bffa5fc52
Marco Arnone <marcoarnone@easy-network.it>
2019-01-10 10:10:21 +0100

    exercises complited

--- src/exercises/MyListGenerics2.scala
+++ src/exercises/MyListGenerics2.scala
@@ -10,6 +10,11 @@ abstract class MyListGenerics2[+A] {
   override def toString= "[" + printElements + "]"
 
   def map[B](transformer: MyTransformer[A, B]) : MyListGenerics2[B]
+  def flatMap[B](transformer: MyTransformer[A, MyListGenerics2[B]]): MyListGenerics2[B]
+  def filter(predicate: MyPredicate[A]): MyListGenerics2[A]
+
+  // concatenation
+  def ++[B >: A](list: MyListGenerics2[B]): MyListGenerics2[B]
 }
 
 object EmptyListGenerics2 extends MyListGenerics2[Nothing] {
@@ -18,6 +23,12 @@ object EmptyListGenerics2 extends MyListGenerics2[Nothing] {
   def isEmpty: Boolean = true
   def add[B >: Nothing](element: B): MyListGenerics2[B] = new ConsGenerics2[B](element, EmptyListGenerics2)
   def printElements: String = ""
+
+  def map[B](transformer: MyTransformer[Nothing, B]) : MyListGenerics2[B] = EmptyListGenerics2
+  def flatMap[B](transformer: MyTransformer[Nothing, MyListGenerics2[B]]): MyListGenerics2[B] = EmptyListGenerics2
+  def filter(predicate: MyPredicate[Nothing]): MyListGenerics2[Nothing] = EmptyListGenerics2
+
+  def ++[B >: Nothing](list: MyListGenerics2[B]): MyListGenerics2[B] = list
 }
 
 class ConsGenerics2[+A] (h: A, t: MyListGenerics2[A]) extends MyListGenerics2[A] {
@@ -29,6 +40,19 @@ class ConsGenerics2[+A] (h: A, t: MyListGenerics2[A]) extends MyListGenerics2[A]
   override def printElements: String =
     if(t.isEmpty) "" + h
     else h + " " + tail.printElements
+
+  def map[B](transformer: MyTransformer[A, B]) : MyListGenerics2[B] =
+    new ConsGenerics2(transformer.transform(h), t.map(transformer))
+
+  def flatMap[B](transformer: MyTransformer[A, MyListGenerics2[B]]): MyListGenerics2[B] =
+    transformer.transform(h) ++ t.flatMap(transformer)
+
+  def filter(predicate: MyPredicate[A]): MyListGenerics2[A] =
+    if(predicate.test(h)) new ConsGenerics2(h, t.filter(predicate))
+    else t.filter(predicate)
+
+  def ++[B >: A](list: MyListGenerics2[B]): MyListGenerics2[B] =
+    new ConsGenerics2(h, t ++ list)
 }
 
 trait MyPredicate[-T] {
@@ -36,13 +60,28 @@ trait MyPredicate[-T] {
 }
 
 trait MyTransformer[-A, B] {
-  def transform(element: A) : {}
+  def transform(element: A) : B
 }
 
 object ListTestGenerics2 extends App {
   val listOfIntegers : MyListGenerics2[Int] = new ConsGenerics2(1, new ConsGenerics2(2, new ConsGenerics2(3, EmptyListGenerics2)))
+  val anotherListOfIntegers : MyListGenerics2[Int] = new ConsGenerics2(4, new ConsGenerics2(5, EmptyListGenerics2))
   val listOfString: MyListGenerics2[String] = new ConsGenerics2("Hello", new ConsGenerics2("Scala", EmptyListGenerics2))
 
   println(listOfIntegers.toString)
   println(listOfString.toString)
+
+  println(listOfIntegers.map(new MyTransformer[Int, Int] {
+    override def transform(element: Int): Int = element * 2
+  }).toString)
+
+  println(listOfIntegers.filter(new MyPredicate[Int] {
+    override def test(element: Int): Boolean = element % 2 == 0
+  }).toString)
+
+  println((listOfIntegers ++ anotherListOfIntegers).toString)
+  println(listOfIntegers.flatMap(new MyTransformer[Int, MyListGenerics2[Int]] {
+    override def transform(element: Int): MyListGenerics2[Int] =
+      new ConsGenerics2[Int](element, new ConsGenerics2[Int](element + 1, EmptyListGenerics2))
+  }).toString)
 }
