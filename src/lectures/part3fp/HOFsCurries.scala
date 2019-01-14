d270a43841ccc99ab2d35f8305984fd8d4629482
Marco Arnone <marcoarnone@easy-network.it>
2019-01-14 12:33:00 +0100

    curries

---
+++ src/lectures/part3fp/HOFsCurries.scala
@@ -0,0 +1,41 @@
+package lectures.part3fp
+
+object HOFsCurries extends App {
+
+  val superFunction: (Int, (String, (Int => Boolean)) => Int) => (Int => Int) = null
+  // higher order function (HOF)
+
+  // map, flatMap, filter in MyList
+
+  // function that applies a function n times over a value x
+
+  def nTimes(f: Int => Int, n: Int, x: Int): Int =
+    if(n <= 0) x
+    else nTimes(f, n - 1, f(x))
+
+  val plusOne = (x: Int) => x + 1
+
+  println(nTimes(plusOne, 10, 1))
+
+  def nTimesBetter(f: Int => Int, n: Int): (Int => Int) =
+    if(n <= 0) (x: Int) => x
+    else (x: Int) => nTimesBetter(f, n - 1)(f(x))
+
+  val plus10 = nTimesBetter(plusOne, 10)
+
+  println(plus10(1))
+
+  // curried functions
+  val superAdder: Int => (Int => Int) = (x: Int) => (y: Int) => x + y
+  val add3 = superAdder(3)
+  println(add3(10))
+
+  // functions with multiple parameter lists
+  def curriedFormatter(c: String)(x: Double): String = c.format(x)
+
+  val standardFormat: (Double => String) = curriedFormatter("%4.2f")
+  val preciseFormat: (Double => String) = curriedFormatter("%10.8f")
+
+  println(standardFormat(Math.PI))
+  println(preciseFormat(Math.PI))
+}
